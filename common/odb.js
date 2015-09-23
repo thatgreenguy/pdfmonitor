f71aa03e23b5a6be2bce50afe7902d21c9e3640f
Paul Green <thatgreenguy@gmx.co.uk>
2015-09-23 13:30:21 +0100

    restructure due to issues with long running process and DB resources

--- common/odb.js
+++ common/odb.js
@@ -41,55 +41,160 @@ module.exports.poolTimeout;
 
 //
 // Create a connection Pool.
-module.exports.createPool = function( ) {
+module.exports.createPool = function( cb ) {
 
+  oracledb.createPool( credentials, function( err, cb ) {
+
+    if ( err ) {
+ 
+      log.error( 'createPool: FAILED: to create a new oracle DB connection Pool' );
+      log.error( err );
+      cb( err, null );      
+
+    } else {
+
+      log.debug( 'createPool: OK' );
+      log.debug( pool );
+      cb( null, pool );      
+
+    }
+  }
 }
 
 
 //
 // Terminate a connection Pool.
-module.exports.terminatePool = function( ) {
+module.exports.terminatePool = function( pool, cb ) {
 
+  pool.terminate( function( err ) {
+
+    if ( err ) {
+
+      log.error( 'terminatePool: FAILED: to terminate the passed connection pool' );
+      log.error( err );
+      cb( err );
+
+    } else {
+
+      log.debug( 'terminatePool: OK' );
+      cb( null );
+
+    }
+  }
 }
 
 
 //
 // Get a connection from the Pool.
-module.exports.getConnection = function( ) {
+module.exports.getConnection = function( pool, cb ) {
 
+  pool.getConnection( function( err, connection ) {
+
+    if ( err ) {
+
+      log.verbose( 'getConnection: FAILED: to get a Connection from the passed Pool' );
+      log.error( err );
+      cb( err, null );
+ 
+    } else {
+
+      log.debug( 'getConnection: OK' );
+      cb( null, connection ); 
+
+    }
+  });
 }
 
 
 //
 // Release a connection back to the Pool.
-module.exports.releaseConnection = function( ) {
+module.exports.releaseConnection = function( connection, cb ) {
 
+  connection.release( function( err ) {
+
+    if ( err ) {
+
+      log.error( 'releaseConnection: FAILED: to release connection' );
+      log.error( err );
+      cb( err, null );
+
+    } else {
+
+      log.debug( 'releaseConnection: OK' );
+      cb( null ); 
+
+    }
+  });
 }
 
 
 //
-// Perform an Insert query statement.
-module.exports.performInsert = function( ) {
+// Perform an adhoc SQL query Statement.
+module.exports.performSQL = function( connection, query, binds, options, cb ) {
 
+  log.debug( 'performSQL: ' + query );
+
+  connection.execute( queryStatement, binds, options, function( err, result ) {
+
+    if ( err ) {
+
+      log.warn( 'performSQL FAILED : ' );
+      log.error( err );
+      cb( err, null );  
+
+    } else {
+
+      log.debug( 'performSQL: OK' );
+      cb( null, result ); 
+
+    }
+  });
 }
+
+
 
 
 //
 // Perform a select query statement.
-module.exports.performDelete = function( ) {
+module.exports.performSelect = function( connection, cb ) {
 
+  log.debug( 'performSelect: ' + sql );
+
+  connection.execute( sql, binds, options, function( err, rs ) {
+
+    if ( err ) {
+
+      log.warn( 'performSelect: FAILED : ' );
+      log.error( err );
+      cb( err, null );  
+
+    } else {
+
+      log.debug( 'performSelect: OK' );
+      cb( null, result ); 
+
+    }
+  });
 }
 
 
 //
-// Perform a select query statement.
-module.exports.performSelect = function( ) {
+// Close the passed selection result set.
+module.exports.closeSelectSet = function( connection, rs, cb ) {
 
-}
+  rs.close( function( err ) {
 
+    if ( err ) {
 
-//
-// Close a selection resultset.
-module.exports.closeSelectResult = function( ) {
+      log.error( 'closeSelectSet: FAILED : ' );
+      log.error( err );
+      module.exports.releaseConnection( connection, cb );
 
+    } else {
+
+      log.debug( 'closeSelectSet: OK ' );
+      cb( null );
+
+    }
+  });
 }
